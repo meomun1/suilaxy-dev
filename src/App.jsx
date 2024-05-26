850485cb4d1f855b72b39585841b8b0ce56da3b2
Le Tien Phat <91601109+Niko1444@users.noreply.github.com>
2024-05-26 18:11:24 +0700

    fix import App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useRef } from 'react'
 import { PhaserGame } from './game/PhaserGame'
-import { ConnectButton , useCurrentAccount, useSuiClientQuery} from '@mysten/dapp-kit';
+import {
+	ConnectButton,
+	useCurrentAccount,
+	useSuiClientQuery,
+} from '@mysten/dapp-kit'
 
 function App() {
 	//  References to the PhaserGame component (game and scene are exposed)
@@ -19,56 +23,58 @@ function App() {
 		console.log(scene)
 	}
 
-    return (
-
-            <div id="app">
-                <PhaserGame ref={phaserRef} currentActiveScene={currentScene} />
-                <div>
-                    <div>
-                        <button className="button" onClick={changeScene}>
-                            Change Scene
-                        </button>
-						<ConnectButton />
-						<ConnectedAccount />
-
-                    </div>
-                </div>
-            </div>
-    );
+	return (
+		<div id="app">
+			<PhaserGame ref={phaserRef} currentActiveScene={currentScene} />
+			<div>
+				<div>
+					<button className="button" onClick={changeScene}>
+						Change Scene
+					</button>
+					<ConnectButton />
+					<ConnectedAccount />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 function ConnectedAccount() {
-    const account = useCurrentAccount();
+	const account = useCurrentAccount()
 
-    if (!account) {
-        return null;
-    }
+	if (!account) {
+		return null
+	}
 
-    return (
-        <div>
-            <div>Connected to {account.address}</div>
-            <OwnedObjects address={account.address} />
-        </div>
-    );
+	return (
+		<div>
+			<div>Connected to {account.address}</div>
+			<OwnedObjects address={account.address} />
+		</div>
+	)
 }
 
 function OwnedObjects(props) {
-    const { data } = useSuiClientQuery('getOwnedObjects', {
-        owner: props.address,
-    });
-    if (!data) {
-        return null;
-    }
+	const { data } = useSuiClientQuery('getOwnedObjects', {
+		owner: props.address,
+	})
+	if (!data) {
+		return null
+	}
 
-    return (
-        <ul>
-            {data.data.map((object) => (
-                <li key={object.data?.objectId}>
-                    <a href={`https://example-explorer.com/object/${object.data?.objectId}`}>
-                        {object.data?.objectId}
-                    </a>
-                </li>
-            ))}
-        </ul>
-    );
+	return (
+		<ul>
+			{data.data.map((object) => (
+				<li key={object.data?.objectId}>
+					<a
+						href={`https://example-explorer.com/object/${object.data?.objectId}`}
+					>
+						{object.data?.objectId}
+					</a>
+				</li>
+			))}
+		</ul>
+	)
 }
+
+export default App

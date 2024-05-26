6bec000115c34d4fe8e4c3ad68db649236f17edc
Le Tien Phat <91601109+Niko1444@users.noreply.github.com>
2024-05-26 23:57:37 +0700

    base notification

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-import '@mysten/dapp-kit/dist/index.css';
+import '../public/input.scss'
+import '@mysten/dapp-kit/dist/index.css'
 
-import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
-import { getFullnodeUrl } from '@mysten/sui.js/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit'
+import { getFullnodeUrl } from '@mysten/sui.js/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 const networks = {
 	devnet: { url: getFullnodeUrl('devnet') },
 	mainnet: { url: getFullnodeUrl('mainnet') },
-};
+}
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root')
 
 if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(
-        <React.StrictMode>
-            <QueryClientProvider client={queryClient}>
-                <SuiClientProvider networks={networks} defaultNetwork="devnet">
-                    <WalletProvider>
-                        <App />
-                    </WalletProvider>
-                </SuiClientProvider>
-            </QueryClientProvider>
-        </React.StrictMode>,
-    );
-  } else {
-    console.error("Could not find root element");
-  }
+	ReactDOM.createRoot(rootElement).render(
+		<React.StrictMode>
+			<QueryClientProvider client={queryClient}>
+				<SuiClientProvider networks={networks} defaultNetwork="devnet">
+					<WalletProvider>
+						<App />
+					</WalletProvider>
+				</SuiClientProvider>
+			</QueryClientProvider>
+		</React.StrictMode>,
+	)
+} else {
+	console.error('Could not find root element')
+}

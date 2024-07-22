be3d99f2edaf78b0d522fc91ff47458b0b16481a
meomun1 <maluong1504@gmail.com>
2024-07-22 13:33:00 +0700

    Update port

--- src/game/objects/Socket.js
+++ src/game/objects/Socket.js
@@ -2,8 +2,7 @@ import io from 'socket.io-client'
 
 // Assuming the intention is to dynamically select the PORT based on some condition
 // For example, checking if the app is running in a development environment
-let PORT =
-	'http://localhost:3000/' || 'https://render-socket-t2rl7mbmfa-as.a.run.app/'
+let PORT = 'https://render-socket-t2rl7mbmfa-as.a.run.app/'
 
 const socket = io(PORT)
 

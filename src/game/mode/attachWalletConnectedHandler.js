979123500dfc29d4df83911377419c8788ea2e29
meomun1 <maluong1504@gmail.com>
2025-01-23 16:28:44 +0700

    Hot fix handler

--- src/game/mode/attachWalletConnectedHandler.js
+++ src/game/mode/attachWalletConnectedHandler.js
@@ -1,8 +1,9 @@
 import { resetEverything } from '../utils/adjustStats'
 import { shutdown } from '../utils/endGamescene'
 import MenuScreen from '../scenes/MenuScreen'
+import SelectUtility from '../scenes/SelectUtility'
 
-const handleWalletConnected = (data) => {
+function handleWalletConnected(data) {
 	if (!data.connected) {
 		if (this.sys.game.globals.bgMusic) {
 			this.sys.game.globals.bgMusic.stop()
